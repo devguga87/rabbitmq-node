f5a55f65bc988851e8fe858b9779d321e61de733
Gustavo Seabra <devguga87@gmail.com>
2025-08-24 22:46:46 -0300

    feat: cria app para utilizar rabbitmqrabbit mq

---
+++ src/01-connection/connection.ts
@@ -0,0 +1,24 @@
+import * as amqp from 'amqplib';
+
+const sleep = (ms:number) => new Promise(resolve => setTimeout(resolve, ms));
+
+async function connect(){
+    try {
+        const connection = await amqp.connect('amqp://admin:admin@localhost:5672');
+        console.log('Conectado ao RabbitMQ com sucesso!');
+
+        const channel = await connection.createChannel();
+        console.log('Canal criado com sucesso');
+
+        await sleep(30000);
+
+        //fechar conexao apos uso
+        await channel.close();
+        await connection.close();
+        console.log('Conexão fechada');
+    } catch (error){
+        console.error('Erro na conexao com o RabbitMQ: ', error);
+    }
+}
+
+connect();
---
+++ src/02-consumer-producer/consumer.ts
@@ -0,0 +1,21 @@
+import amqp from "amqplib";
+
+async function consumer(){
+    const connection = await amqp.connect("amqp://admin:admin@localhost:5672");
+    const channel = await connection.createChannel();
+
+    const queue = "hello";
+
+    await channel.assertQueue(queue);
+    console.log(`[*] Waiting for messagens in ${queue} To exit press CTRL+C`);
+
+    channel.consume(
+        queue,
+        (msg) => {
+            console.log(`[x] Received ${msg?.content.toString()}`)
+        },
+        { noAck: true}
+    )
+}
+
+consumer().catch(console.error)
